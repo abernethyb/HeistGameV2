db22f02ccbe568eb920b53c5b811294fdca7a968
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-09-04 12:57:01 -0500

    added robbers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace heistV2
 {
@@ -6,7 +7,39 @@ namespace heistV2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Hacker owl = new Hacker()
+            {
+                name = "Owl",
+                SkillLevel = 75,
+                PercentageCut = 25
+            };
+            Hacker eeyore = new Hacker()
+            {
+                name = "Eeyore",
+                SkillLevel = 75,
+                PercentageCut = 1
+            };
+            LockSpecialist piglet = new LockSpecialist()
+            {
+                name = "Piglet",
+                SkillLevel = 25,
+                PercentageCut = 15
+            };
+            Muscle pooh = new Muscle()
+            {
+                name = "Pooh",
+                SkillLevel = 75,
+                PercentageCut = 50
+            };
+
+            List<IRobber> rolodex = new List<IRobber>()
+            {
+                owl,
+                eeyore,
+                piglet,
+                pooh
+            };
         }
     }
 }
